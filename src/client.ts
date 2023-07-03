bc1cf4aa56b2155649fd9dae22726da2e5b768e6
uvorbs <yuval@orbs.com>
2023-07-03 15:15:11 +0300

    merged 2

--- src/client.ts
+++ src/client.ts
@@ -9,19 +9,6 @@
 import { LiteClient, LiteRoundRobinEngine, LiteSingleEngine } from 'ton-lite-client';
 import { fetchConfig } from './utils/fetchConfig';
 
-function shuffleArray(array: any[]): any[] {
-    // Clone the original array to avoid modifying the original
-    const shuffledArray = [...array];
-
-    // Start from the last element and swap it with a random element before it
-    for (let i = shuffledArray.length - 1; i > 0; i--) {
-        const randomIndex = Math.floor(Math.random() * (i + 1));
-        [shuffledArray[i], shuffledArray[randomIndex]] = [shuffledArray[randomIndex], shuffledArray[i]];
-    }
-
-    return shuffledArray;
-}
-
 export async function createClient() {
 
     // Fetch config
@@ -29,7 +16,6 @@ export async function createClient() {
         console.warn('Unable to find TON_CONFIG');
         return null;
     }
-    console.log('fetch config from: ', process.env.TON_CONFIG);
     let config = await fetchConfig(process.env.TON_CONFIG);
     if (config.length === 0) {
         console.warn('No lite servers in config');
@@ -42,34 +28,10 @@ export async function createClient() {
         parallelClients = parseInt(process.env.TON_THREADS, 10);
     }
 
-    // filter out live config only
-    let liveConfig = [];
-
-    for (let c of config) {
-        let engine: LiteSingleEngine | undefined;
-        try {
-            engine = new LiteSingleEngine({ host: c.ip, port: c.port, publicKey: c.key });
-            let client = new LiteClient({ engine, batchSize: 10 });
-            const working: any = await client.getMasterchainInfo()
-            // .catch(e => {
-            //     engine.close();
-            //     console.error('getMasterchainInfo', e);
-            // });
-            if (working.kind) {
-                liveConfig.push(c)
-            }
-        } catch (e) {
-            console.error(e, `Failed to connect to ${c.ip}:${c.port}`);
-        }
-        engine?.close();
-    }
-    // randomise live config so not all v4 instances would work with the same lite server
-    liveConfig = shuffleArray(liveConfig);
-
     // Create engines
     let commonClientEngines: LiteSingleEngine[] = [];
     let child: { clients: LiteClient[] }[] = []
-    for (let c of liveConfig) {
+    for (let c of config) {
         let clients: LiteClient[] = [];
         for (let i = 0; i < parallelClients; i++) {
             let engine = new LiteSingleEngine({ host: `https://${c.ip}:${c.port}`, publicKey: c.key });
